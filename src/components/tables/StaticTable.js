70bca8cc8b7391f185757c186554c23e082814e2
yagoubigithub <yagoubi.aek.2@gmail.com>
2020-06-15 10:51:07 +0200

    a

--- src/components/tables/StaticTable.js
+++ src/components/tables/StaticTable.js
@@ -3,18 +3,70 @@ import React, { Component } from "react";
 import ReactTable from "react-table";
 
 export default class StaticTable extends Component {
+  state = {
+    defaultColumns: [
+      "Nom",
+      "Prénom",
+      "Adresse",
+      "Télephone",
+      "Email",
+      "Date de Début",
+      "Date de fin",
+    ],
+    choose: [],
+    selectValue: {},
+  };
 
-    state = {
-        defaultColumns : ["Nom", "Prénom" ,  "Adresse", "Télephone" , "Email", "Date de Début", "Date de fin"],
-       
+  componentDidMount() {
+    const selectValue = { ...this.state.selectValue };
+    Object.keys(this.props.rows[0]).map((key) => {
+      selectValue[key] = "0";
+    });
+    this.setState({
+      selectValue,
+    });
+  }
+  ifTitleExist = (selectValue,title) =>{
+
+    let exist = false;
+    Object.keys(selectValue).map(key=>{
+      if(selectValue[key] === title){
+        exist = true;
+        return;
+      }
+
+    })
+
+    return exist;
+  }
+  handleSelectChange = (e, key) => {
+    const title = e.target.value;
+    
+    const choose = [...this.state.choose];
+    const selectValue = { ...this.state.selectValue };
+
+    if(this.ifTitleExist(selectValue,title)){
+      return;
+    }else{
+      const index = choose.findIndex(function (value) {
+        return value.title === title;
+      });
+      if (index !== -1) {
+        choose.splice(index, 1);
+      }
+      choose.push({
+        title,
+        key,
+      });
+  
+      selectValue[key] = title;
+  
+      this.setState({ choose, selectValue });
+  
+      console.log(choose);
     }
-  /*   handleSelectChange = (e) =>{
-        const title = e.target.value;
-        const defaultColumns = [...this.state.defaultColumns];
-        const selectValue = defaultColumns[defaultColumns.indexOf(title)];
-        defaultColumns.splice(defaultColumns.indexOf(title),1);
-        this.setState({defaultColumns, selectValue})
-    } */
+  
+  };
   render() {
     const rows = this.props.rows;
 
@@ -22,43 +74,46 @@ export default class StaticTable extends Component {
       return {
         Header: key,
         accessor: key,
-        width : 200,
+        width: 200,
         filterMethod: (filter, row) => {
-        
           return true;
         },
         Cell: (props) => {
-            if(props.value !== undefined){
-               
-                const render =  isNaN( Date.parse(props.value) ) ? props.value : props.value.toString().split('T')[0]
-                return(
-                    <div className="cell">
-                      {props.value !== "undefined" ? render : ""}
-                    </div>
-                )
-            }
-            return(<div className="cell"></div>)
-            
-           
-            
-           
+          if (props.value !== undefined) {
+            const render = isNaN(Date.parse(props.value))
+              ? props.value
+              : props.value.toString().split("T")[0];
+            return (
+              <div className="cell">
+                {props.value !== "undefined" ? render : ""}
+              </div>
+            );
+          }
+          return <div className="cell"></div>;
         },
-        Filter: ({ filter, onChange }) => (
+        Filter: ({ filter, onChange }) => {
+          return (
             <div className="searchtable-container">
-            <select >
-            <option value="0">Selectionner une titre</option>
-                {this.state.defaultColumns.map((title, index)=>{
-                    return(<option key={index} value={title}>{title}</option>)
+              <select
+                onChange={(e) => this.handleSelectChange(e, key)}
+                value={this.state.selectValue[key]}
+              >
+                <option value="0">Selectionner une titre</option>
+                {this.state.defaultColumns.map((title, index) => {
+                  return (
+                    <option key={index} value={title}>
+                      {title}
+                    </option>
+                  );
                 })}
-            </select>
+              </select>
             </div>
-          )
-
-
+          );
+        },
       };
     });
     return (
-      <div >
+      <div>
         <ReactTable
           className="table"
           data={rows}
